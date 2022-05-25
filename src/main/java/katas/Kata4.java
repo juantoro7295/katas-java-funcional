343419b2f709eb13e720eddd50a512b07dae3f76
juantoro7295 <juanpatoro2011@gmail.com>
2022-05-25 13:49:33 -0500

    Kata 4 - Solucion
    Se aplana la lista ,se utiliza el map, se crea un nuevo constructor, se muestran los item necesesarios, se utiliza el filter para la condicion de boxArt se muestra el elemento 1 de la secuencia , con el buider se devuelve el mapa inmutable creado, y se usa el collect para pasar de stream a list

--- src/main/java/katas/Kata4.java
+++ src/main/java/katas/Kata4.java
@@ -2,12 +2,14 @@ package katas;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.sun.source.tree.LiteralTree;
 import model.BoxArt;
 import model.MovieList;
 import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve id, title, and a 150x200 box art url for every video
@@ -17,7 +19,19 @@ import java.util.Map;
 public class Kata4 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
+        List<Map> copiaMovies = movieLists.stream().flatMap((list) -> list.getVideos().stream())
+                .map((item) -> ImmutableMap.builder()
+                        .put("id: ", item.getId())
+                        .put("title: ", item.getTitle())
+                        .put("boxArt: ", item.getBoxarts().stream()
+                                .filter((filter) -> filter.getWidth() == 150 && filter.getHeight() == 200)
+                                .findFirst()).build())
+                .collect(Collectors.toList());
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", new BoxArt(150, 200, "url")));
+        return copiaMovies;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(execute());
     }
 }
