7ad3e7879fa49e4a8063c541d17b9a81250375b1
juantoro7295 <juanpatoro2011@gmail.com>
2022-05-25 15:09:11 -0500

    Kata 6 - Solucion
    Se utiliza el flatMap para aplanar la lista, se utilice el reduce para cumplir la condicion de la url mas grande y se proyecta con el map

--- src/main/java/katas/Kata6.java
+++ src/main/java/katas/Kata6.java
@@ -14,6 +14,17 @@ public class Kata6 {
     public static String execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return "someUrl";
+        String urlMasGrande = movies
+                .stream().flatMap((list)-> list.getBoxarts().stream())
+                .reduce((wh1,wh2)-> wh1.getWidth()* wh1.getHeight() > wh2.getWidth()* wh2.getHeight()
+                        ? wh1 : wh2).map((item)-> "URL: " + item.getUrl()).toString();
+
+
+
+        return urlMasGrande;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(execute());
     }
 }
