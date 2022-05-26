9ab7e6be22560068cc165b5c7ae1bc6bad5cd3ee
juantoro7295 <juanpatoro2011@gmail.com>
2022-05-25 20:52:29 -0500

    Kata 9 - Solucion
    Se aplana la lista, se inicia el constructor, se muestran los item necesarios, se utiliza el reduce para cumplir condicion del tiempo, y tambien se utiliza reduce para cumplir condicion de boxArt, se utiliza el collect para pasar de stream a list

--- src/main/java/katas/Kata9.java
+++ src/main/java/katas/Kata9.java
@@ -10,6 +10,7 @@ import util.DataUtil;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve each video's id, title, middle interesting moment time, and smallest box art url
@@ -20,6 +21,27 @@ public class Kata9 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "some title", "time", new Date(), "url", "someUrl"));
+        List<Map> copiaMovies = movieLists.stream().flatMap((list) -> list.getVideos().stream())
+                .map((item) -> ImmutableMap.builder()
+                        .put("idVideo: ", item.getId())
+                        .put("title: ", item.getTitle())
+                        .put("tiempo: ", item.getInterestingMoments()
+                                .stream()
+                                .filter((item2) -> item2.getType().equals("Middle")).findFirst()
+                                .get()
+                                .getTime()
+                                .toString())
+                        .put("url: ", item.getBoxarts()
+                                .stream()
+                                .reduce((wh1, wh2) -> wh1.getWidth() * wh1.getHeight() < wh2.getWidth() * wh2.getHeight()
+                                        ? wh1 : wh2).get().getUrl())
+                        .build()).collect(Collectors.toList());
+
+
+        return copiaMovies;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(execute());
     }
 }
